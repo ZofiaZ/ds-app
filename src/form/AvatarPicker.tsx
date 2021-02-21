54b2edfee20c491085689ce8cebfe02dcb870243
Zofia Zmojdzin <zosiazmojdzin@gmail.com>
2021-02-21 10:06:50 +0100

    preview selected avatar

--- src/form/AvatarPicker.tsx
+++ src/form/AvatarPicker.tsx
@@ -1,40 +1,56 @@
 import React, { useState } from "react";
 import { AvatarPickerDialog } from "@atlaskit/media-avatar-picker";
+import Avatar from "@atlaskit/avatar";
 import { ModalTransition } from "@atlaskit/modal-dialog";
 import Button from "@atlaskit/button";
+import { Field } from "@atlaskit/form";
 
 // const avatars: Array<Avatar> = [{ dataURI: "some-data-uri" }];
 
 const AvatarPicker = () => {
   const [isOpen, setIsOpen] = useState(false);
+  const [imageDataURI, setImageDataURI] = useState("");
 
   return (
-    <>
-      <Button
-        appearance="primary"
-        onClick={() => {
-          setIsOpen(true);
-        }}
-      >
-        Upload Profile Picture
-      </Button>
-      <ModalTransition>
-        {isOpen && (
-          <AvatarPickerDialog
-            avatars={[]}
-            onImagePicked={(selectedImage, crop) => {
-              console.log(selectedImage.size, crop.x, crop.y, crop.size);
-            }}
-            onAvatarPicked={(selectedAvatar) =>
-              console.log(selectedAvatar.dataURI)
-            }
-            onCancel={() => {
-              setIsOpen(false);
-            }}
-          />
-        )}
-      </ModalTransition>
-    </>
+    <Field name="avatar" defaultValue="" label="avatar">
+      {({ fieldProps }) => (
+        <>
+          <div className="AvatarPicker-container">
+            <Avatar src={imageDataURI} size="xlarge" />
+            <Button
+              onClick={() => {
+                setIsOpen(true);
+              }}
+            >
+              change image
+            </Button>
+          </div>
+          <ModalTransition>
+            {isOpen && (
+              <AvatarPickerDialog
+                avatars={[]}
+                onImagePicked={(selectedImage, crop) => {
+                  console.log("onImagePicked");
+                }}
+                onAvatarPicked={(selectedAvatar) => {
+                  console.log("onAvatarPicked", selectedAvatar.dataURI);
+                  setImageDataURI(selectedAvatar.dataURI);
+                }}
+                onImagePickedDataURI={(exportedImg) => {
+                  console.log("onImagePickedDataURI");
+                  setImageDataURI(exportedImg);
+                  fieldProps.onChange(exportedImg);
+                  setIsOpen(false);
+                }}
+                onCancel={() => {
+                  setIsOpen(false);
+                }}
+              />
+            )}
+          </ModalTransition>
+        </>
+      )}
+    </Field>
   );
 };
 
