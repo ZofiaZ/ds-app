564f15b0ebf76cc58431242fa23b5419f31a172a
Zofia Zmojdzin <zosiazmojdzin@gmail.com>
2021-02-25 17:24:16 +0100

    add length validation to textarea

--- src/components/form/TextAreaField.tsx
+++ src/components/form/TextAreaField.tsx
@@ -1,11 +1,26 @@
 import React, { FunctionComponent } from "react";
 import { Field, ErrorMessage } from "@atlaskit/form";
 import TextArea from "@atlaskit/textarea";
+import * as VALIDATION_TYPES from "../../utils/validationTypes";
 
 type PropTypes = {
   name: string;
   label: string;
   defaultValue?: string;
+};
+
+const maxCharacters = 100;
+
+const validate = (value?: string) => {
+  if (!value || !value.trim()) {
+    return;
+  }
+
+  if (maxCharacters && value.trim().length > maxCharacters) {
+    return VALIDATION_TYPES.TOO_LONG;
+  }
+
+  return;
 };
 
 const TextAreaField: FunctionComponent<PropTypes> = ({
@@ -18,11 +33,19 @@ const TextAreaField: FunctionComponent<PropTypes> = ({
     label={label}
     defaultValue={defaultValue}
     isRequired
+    validate={validate}
   >
     {({ fieldProps, error }) => (
       <>
         <TextArea {...fieldProps} />
-        {error && <ErrorMessage>{error}</ErrorMessage>}
+        {error === VALIDATION_TYPES.TOO_LONG && (
+          <ErrorMessage>
+            {label} needs to have maximum {maxCharacters} characters
+          </ErrorMessage>
+        )}
+        {error === VALIDATION_TYPES.REQUIRED && (
+          <ErrorMessage>this field cannot be blank</ErrorMessage>
+        )}
       </>
     )}
   </Field>
